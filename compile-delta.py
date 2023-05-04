1cff6e567d9c7db5e780ba909ca43ff7dccf0e17
Bülent Özden <bulentozden2007@gmail.com>
2023-05-04 23:34:34 +0300

    Add initial compile-delta.py

---
+++ compile-delta.py
@@ -0,0 +1,179 @@
+###########################################################################
+# compile-delta.py
+#
+# Compile a delta dataset for whisper supported languages
+# - Take difference of validated.tsv in v13.0 and validated.tsv in v9.0 (whisper is based on this)
+# - Sort text by desc length
+# - Take the longest MAX_DELTA_SIZE (default 1000) unique texts
+# - Output to data/cv-delta/[lc]/delta.tsv
+# - Copy related audio to data/cv-delta/[lc]/clips/*.mp3
+#
+# This script is part of Common Voice ToolBox Package
+#
+# github: https://github.com/HarikalarKutusu/cv-whisper-performance
+# Copyright: (c) Bülent Özden, License: AGPL v3.0
+###########################################################################
+
+import sys, os, shutil, glob, csv
+from datetime import datetime, timedelta
+from typing import Any
+from collections import Counter
+
+import numpy as np
+import pandas as pd
+
+# MultiProcessing
+import multiprocessing as mp
+import psutil
+
+# Common Voice Utilities
+import cvutils as cvu
+
+HERE: str = os.path.dirname(os.path.realpath(__file__))
+if not HERE in sys.path:
+    sys.path.append(HERE)
+
+import config as conf
+import const as c
+
+#
+# Constants
+#
+
+# Program parameters
+VERBOSE: bool = False
+FAIL_ON_NOT_FOUND: bool = True
+
+# Multi Processing Cores
+NUM_PROCS: int = psutil.cpu_count(logical=False) - 1
+
+#
+# Support functions
+#
+
+def df_read(fpath: str) -> pd.DataFrame:
+    """Read a tsv file into a dataframe"""
+    if not os.path.isfile(fpath):
+        print(f'FATAL: File {fpath} cannot be located!')
+        if FAIL_ON_NOT_FOUND:
+            sys.exit(1)
+
+    df: pd.DataFrame = pd.read_csv(
+        fpath,
+        sep="\t",
+        parse_dates=False,
+        engine="python",
+        encoding="utf-8",
+        on_bad_lines='skip',
+        quotechar='"',
+        quoting=csv.QUOTE_NONE,
+        dtype={'ver': str}
+    )
+    return df
+
+
+def df_write(df: pd.DataFrame, fpath: str) -> bool:
+    """
+    Writes out a dataframe to a file.
+    """
+    # Create/override the file
+    df.to_csv(fpath, header=True, index=False, encoding="utf-8", sep='\t', escapechar='\\', quoting=csv.QUOTE_NONE)
+    if VERBOSE:
+        print(f'Generated: {fpath} Records={df.shape[0]}')
+    return True
+
+#
+# Locale handling (multiprocessed)
+#
+
+def handle_locale(lc: str) -> None:
+    """Handle a single locale (multiprocess)"""
+    # precalc paths
+    cv9_path = os.path.join(conf.CV9_DIR, lc)
+    cv_latest_path = os.path.join(conf.CV_LATEST_DIR, lc)
+    cv_latest_audio_path = os.path.join(cv_latest_path, "clips")
+
+    # get validated dataframes
+    validated_v9_df: pd.DataFrame = df_read(os.path.join(cv9_path, "validated.tsv"))
+    validated_latest_df: pd.DataFrame = df_read(os.path.join(cv_latest_path, "validated.tsv"))
+
+    # diff
+    diff_df: pd.DataFrame = pd.concat([validated_v9_df, validated_latest_df]).drop_duplicates(keep=False)
+
+    # Get sentence lengths, sort by length desc (we want long recordings)
+    diff_df["s_len"] = diff_df["sentence"].str.len()
+    diff_df.sort_values(["s_len"], ascending=False, inplace=True)
+    diff_df.drop(columns=["s_len"], inplace=True)
+
+    # Remove duplicate sentences & voices for less bias - if required (set in config.py)
+    # These might reduce the size of the test set for 
+    if conf.UNIQUE_SENTENCES:
+        diff_df['s_enum'], s_unique = pd.factorize(diff_df['sentence']) # add an enumaration column for client_id's
+        diff_df.drop_duplicates(subset="s_enum", keep="first", inplace=True)
+        diff_df.drop(columns=["s_enum"], inplace=True)
+
+    if conf.UNIQUE_VOICES:
+        diff_df['v_enum'], v_unique = pd.factorize(diff_df['client_id']) # add an enumaration column for client_id's
+        diff_df.drop_duplicates(subset="v_enum", keep="first", inplace=True)
+        diff_df.drop(columns=["v_enum"], inplace=True)
+
+    # Get top
+    diff_df = diff_df.head(conf.MAX_DELTA_SIZE)
+
+    # create destination
+    dest_path = os.path.join(HERE, "data", "cv-delta", lc)
+    dest_clips_path = os.path.join(dest_path, "clips")
+    os.makedirs(dest_clips_path, exist_ok=True)
+
+    # save diff.tsv
+    df_write(diff_df, os.path.join(dest_path, "diff.tsv"))
+
+    # Copy related clip audio files
+    clip_names: list[str] = diff_df["path"].to_list()
+    for clip_name in clip_names:
+        shutil.copy(os.path.join(cv_latest_audio_path, clip_name), dest_clips_path)
+
+    # Report results
+    result = {
+        "lc": lc,
+        "recordings": diff_df.shape[0],
+        "uq_voices": len(diff_df["client_id"].unique()),
+        "uq_sentences": len(diff_df["sentence"].unique())
+    }
+    print(f"Finished LC={lc} Added {result['recordings']} recordings from {result['uq_sentences']} sentences and {result['uq_voices']} voices to diff")
+    return result
+
+#
+# MAIN PROCESS
+#
+
+def main() -> None:
+    """Main process which loops through whisper languages and handles locales with MP"""
+
+    print(f'==> Whisper supports {len(c.WHISPER_LC)} locales...')
+    # Make sure all exist
+    lc_list: list[str] = []
+    for lc in c.WHISPER_LC:
+        cv9_path = os.path.join(conf.CV9_DIR, lc)
+        cv_latest_path = os.path.join(conf.CV_LATEST_DIR, lc)
+        if os.path.isdir(cv9_path) and os.path.isdir(cv_latest_path):  # both should exist
+            lc_list.append(lc)
+    print(f'==> Skipping {len(c.WHISPER_LC) - len(lc_list)} locales as they do not exist in CV9 & CV13 datasets...')
+
+    # Test
+    results: list[dict[str, int]] = []
+    # results.append(handle_locale(lc_list[0]))
+
+    # Multiprocess each locale
+    print(f'==> Processing remaining {len(lc_list)} locales...')
+    with mp.Pool(NUM_PROCS) as pool:
+        results = pool.map(handle_locale, lc_list)
+
+    results_df = pd.DataFrame.from_records(results, columns=["lc", "recordings", "uq_voices", "uq_sentences"])
+    # print(results)
+    # print(results_df)
+    df_write(results_df, os.path.join(HERE, "data", "cv-delta", "summary.tsv"))
+
+# Entry point
+if __name__ == '__main__':
+    main()
