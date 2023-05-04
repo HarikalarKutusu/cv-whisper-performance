7424de66d34f67138ce01cc357a1336afe8b5f9e
Bülent Özden <bulentozden2007@gmail.com>
2023-05-04 22:09:27 +0300

    Add config & conf

---
+++ config.py
@@ -0,0 +1,28 @@
+###########################################################################
+# config.py
+#
+# Your local configuration
+# Modify the values to point to respective directories in your system
+#
+# This script is part of Common Voice ToolBox Package
+#
+# github: https://github.com/HarikalarKutusu/cv-whisper-performance
+# Copyright: (c) Bülent Özden, License: AGPL v3.0
+###########################################################################
+import os
+
+# Base location of tsv files for Common Voice v9.0
+# CV9_DIR: str = "C:\GITREPO\_HK_GITHUB\_cv_tbox\cv-tbox-dataset-compiler\data\voice-corpus\cv-corpus-9.0-2022-04-27"
+CV9_DIR: str = os.path.join(
+    "C:", os.sep, "GITREPO", "_HK_GITHUB", "_cv_tbox", "cv-tbox-dataset-compiler",
+    "data", "voice-corpus", "cv-corpus-9.0-2022-04-27"
+    )
+
+# Base location of tsv & audio files for Common Voice v13.0
+# CV_LATEST_DIR: str = "M:\DATASETS\CV\cv-corpus-13.0-2023-03-09"
+CV_LATEST_DIR: str = os.path.join(
+    "M:", os.sep, "DATASETS", "CV", "cv-corpus-13.0-2023-03-09"
+    )
+
+# Test set size
+MAX_DELTA_SIZE: int = 1000
---
+++ const.py
@@ -0,0 +1,23 @@
+###########################################################################
+# const.py
+#
+# Constants for scripts in this repository
+#
+# This script is part of Common Voice ToolBox Package
+#
+# github: https://github.com/HarikalarKutusu/cv-whisper-performance
+# Copyright: (c) Bülent Özden, License: AGPL v3.0
+###########################################################################
+
+# Language codes supported by whisper multilingual
+WHISPER_LC: list[str] = [
+    "af", "am", "ar", "as", "az", "ba", "be", "bg", "bn", "bo", "br", "bs",
+    "ca", "cs", "cy", "da", "de", "el", "en", "es", "et", "eu", "fa", "fi", "fo", "fr",
+    "gl", "gu", "ha", "haw", "he", "hi", "hr", "ht", "hu", "hy", "id", "is", "it",
+    "ja", "jw", "ka", "kk", "km", "kn", "ko", "la", "lb", "ln", "lo", "lt", "lv",
+    "mg", "mi", "mk", "ml", "mn", "mr", "ms", "mt", "my", "nb", "ne", "nl", "nn",
+    "oc", "pa", "pl", "ps", "pt", "ro", "ru",
+    "sa", "sd", "si", "sk", "sl", "sn", "so", "sq", "sr", "su", "sv", "sw",
+    "ta", "te", "tg", "th", "tk", "tl", "tr", "tt", "uk", "ur", "uz",
+    "vi", "yi", "yo", "zh"
+    ]
