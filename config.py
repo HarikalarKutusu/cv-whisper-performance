fe0ffb9b7cff530fd00d8d2ee3b74b940fd8de04
Bülent Özden <bulentozden2007@gmail.com>
2023-05-04 23:33:56 +0300

    Add whisper models, path and bias prevention flags

--- config.py
+++ config.py
@@ -11,18 +11,31 @@
 ###########################################################################
 import os
 
+# Test set size
+MAX_DELTA_SIZE: int = 100
+
+# Bias related
+UNIQUE_SENTENCES: bool = True
+UNIQUE_VOICES: bool = False
+
 # Base location of tsv files for Common Voice v9.0
-# CV9_DIR: str = "C:\GITREPO\_HK_GITHUB\_cv_tbox\cv-tbox-dataset-compiler\data\voice-corpus\cv-corpus-9.0-2022-04-27"
 CV9_DIR: str = os.path.join(
     "C:", os.sep, "GITREPO", "_HK_GITHUB", "_cv_tbox", "cv-tbox-dataset-compiler",
     "data", "voice-corpus", "cv-corpus-9.0-2022-04-27"
     )
 
 # Base location of tsv & audio files for Common Voice v13.0
-# CV_LATEST_DIR: str = "M:\DATASETS\CV\cv-corpus-13.0-2023-03-09"
 CV_LATEST_DIR: str = os.path.join(
     "M:", os.sep, "DATASETS", "CV", "cv-corpus-13.0-2023-03-09"
     )
 
-# Test set size
-MAX_DELTA_SIZE: int = 1000
+# Base location of whisper model files
+WHISPER_MODEL_DIR: str = os.path.join(
+    "M:", os.sep, "__STATIC", "_DATASETS", "VOICE", "WHISPER_MODELS"
+    )
+
+# List of whisper models to test against
+WHISPER_MODELS_TEST: list[str] = [
+    "tiny",
+    "base",
+]
