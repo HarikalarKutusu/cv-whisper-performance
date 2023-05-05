d930353386395acaf77c71bfb68ba8816d0f3281
Bülent Özden <bulentozden2007@gmail.com>
2023-05-05 06:55:51 +0300

    Add type classes

--- library.py
+++ library.py
@@ -1,5 +1,8 @@
 import sys, os, shutil, glob, csv
 import pandas as pd
+import whisper
+from typing import TypedDict
+
 import config as conf
 
 def df_read(fpath: str) -> pd.DataFrame:
@@ -32,3 +35,65 @@ def df_write(df: pd.DataFrame, fpath: str) -> bool:
     if conf.VERBOSE:
         print(f'Generated: {fpath} Records={df.shape[0]}')
     return True
+
+#
+# Type definitions
+#
+
+class HandleLocaleProps(TypedDict):
+    model_name: str
+    model: whisper.Whisper
+    diff_path: str
+
+class CommonVoiceRec(TypedDict):
+    client_id: str
+    path: str
+    sentence: str
+    up_votes: int
+    down_votes: int
+    down_votes: int
+    age: str
+    gender:	str
+    accents: str
+    variant: str
+    locale: str
+    segment: str
+
+class WhisperTranscriptionResult(TypedDict):
+    text: str
+    segments: dict
+    language: str
+
+class TranscriptionRec(TypedDict):
+    client_id: str
+    path: str
+    sentence: str
+    up_votes: int
+    down_votes: int
+    down_votes: int
+    age: str
+    gender:	str
+    accents: str
+    variant: str
+    locale: str
+    segment: str
+    transcription: str
+    detected_lc: str
+    cer: float
+    wer: float
+    mer: float
+    wil: float
+    wip: float
+    item_inference_duration: float
+    item_total_duration: float
+
+class AggregationRec(TypedDict):
+    lc: str
+    num_sentences: int
+    inference_duration: float
+    total_duration: float
+    avg_cer: float
+    avg_wer: float
+    avg_mer: float
+    avg_wil: float
+    avg_wip: float
